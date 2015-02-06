a6e2f54884cca3282d8c3cd7da4a92ba0791cf8b
Panos <ptsavdar@gmail.com>
2015-02-06 15:57:42 +0100

    Created data, started stylizing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 (function(){
-	
+    $(document).ready(function() {
+        // Variables
+        var $codeSnippets = $('.code-example-body'),
+            $nav = $('.navbar'),
+            $header = $('header'),
+            $body = $('body'),
+            $window = $(window),
+            navOffsetTop = $nav.offset().top,
+            $document = $(document);
+
+        //Functions 
+        function init() {
+            $window.on('scroll', onScroll);
+            $window.on('resize', resize);
+        }
+
+        function resize() {
+            $header.removeClass('has-docked-nav');
+            navOffsetTop = $nav.offset().top;
+            onScroll();
+        }
+
+        function onScroll() {
+            if(navOffsetTop < $window.scrollTop() && !$header.hasClass('has-docked-nav')) {
+                $header.addClass('has-docked-nav')
+            }
+            if(navOffsetTop > $window.scrollTop() && $header.hasClass('has-docked-nav')) {
+                $header.removeClass('has-docked-nav')
+            }
+        }
+        
+        init();
+    });
 })();
